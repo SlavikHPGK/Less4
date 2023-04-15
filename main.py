e699754838015c24f02ab159d848d146d937dedd
SlavikHPGK <daniloslavik@gmail.com>
2023-04-15 10:13:51 +0300

    init

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import requests
+import json
 
 help(requests)
 r = requests.get('https://randomuser.me/api')
 
 print(r.content)
 person = r.content
+
+print(r.content)
+person = json.loads(r.text)
+
+print(person['results'])
+
+class Person:
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
 
